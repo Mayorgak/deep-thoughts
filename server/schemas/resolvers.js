620ade0ad96fbfb655adcfa00bdd7b0af56d59ca
Kristen M <kristenmayorga@gmail.com>
2020-10-20 14:21:14 -0400

    mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,26 +5,33 @@ const resolvers = {
     thoughts: async () => {
       return Thought.find().sort({ createdAt: -1 });
     },
-thought: async (parent, { _id }) => {
-  return Thought.findOne({ _id });
-},
-  // get all users
-users: async () => {
-  return User.find()
-    .select('-__v -password')
-    .populate('friends')
-    .populate('thoughts');
-},
-// get a user by username
-user: async (parent, { username }) => {
-  return User.findOne({ username })
-    .select('-__v -password')
-    .populate('friends')
-    .populate('thoughts');
-},
+    thought: async (parent, { _id }) => {
+      return Thought.findOne({ _id });
+    },
+    // get all users
+    users: async () => {
+      return User.find()
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    // get a user by username
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select("-__v -password")
+        .populate("friends")
+        .populate("thoughts");
+    },
+    Mutation: {
+      addUser: async (parent, args) => {
+        const user = await User.create(args);
 
-}
-}
+        return user;
+      },
+      login: async () => {},
+    },
+  },
+};
 
 
 module.exports = resolvers;
