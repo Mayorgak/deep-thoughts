78bc8826049391557e1970602526e9a7a0f41123
Kristen M <kristenmayorga@gmail.com>
2020-10-20 15:08:47 -0400

    updated query with me

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,5 @@
 const { User, Thought } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 
 const resolvers = {
   Query: {
@@ -22,13 +23,39 @@ const resolvers = {
         .populate("friends")
         .populate("thoughts");
     },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select("-__v -password")
+          .populate("thoughts")
+          .populate("friends");
+
+        return userData;
+      }
+
+      throw new AuthenticationError("Not logged in");
+    },
     Mutation: {
       addUser: async (parent, args) => {
         const user = await User.create(args);
 
         return user;
       },
-      login: async () => {},
+      login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+
+        const correctPw = await user.isCorrectPassword(password);
+
+        if (!correctPw) {
+          throw new AuthenticationError("Incorrect credentials");
+        }
+
+        return user;
+      },
     },
   },
 };
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,25 +18,26 @@ const typeDefs = gql`
     username: String
   }
   type User {
-  _id: ID
-  username: String
-  email: String
-  friendCount: Int
-  thoughts: [Thought]
-  friends: [User]
-}
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    thoughts: [Thought]
+    friends: [User]
+  }
   type Query {
-  users: [User]
-  user(username: String!): User
-  thoughts(username: String): [Thought]
-  thought(_id: ID!): Thought
-}
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+  }
 
   type Mutation {
+    
     login(email: String!, password: String!): User
     addUser(username: String!, email: String!, password: String!): User
   }
-
 `;
 
 // export the typeDefs
